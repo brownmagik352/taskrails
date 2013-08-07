6ceb8609c87badb24a36f1395416413e543782fb
brownmagik352 <asuman@Apurvs-MacBook-Pro.local>
2013-08-06 23:28:09 -0400

    add basic relationship between user and d task

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,6 @@ class Task < ActiveRecord::Base
 
   validates_presence_of :title, message: "you must enter a title for your task"
   validates_length_of :title, in: 3..20, message: "must be between 3 and 20 characters"
+
+  belongs_to :user
 end
