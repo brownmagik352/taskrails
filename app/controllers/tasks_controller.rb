920ef21b6acbe134943566f064cd63203698ffcd
brownmagik352 <asuman@Apurvs-MacBook-Pro.local>
2013-08-13 21:47:48 -0400

    add flash for create and delete task actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,54 @@
 class TasksController < ApplicationController
 
 	before_filter :get_user
+	before_filter :get_task, :only => [:edit, :update, :show, :destroy]
 
+#Filter Actions
 	def get_user
 		@user = User.find(params[:user_id])
 	end
 
-	before_filter :get_task, :only => [:edit, :update, :show, :destroy]
-
 	def get_task
 		@task = @user.tasks.find(params[:id])
 	end
+
+#RESTful actions
+	def index
+		@tasks = @user.tasks
+	end
+
+	def new
+		@task = Task.new
+		
+	end
+
+	def create
+		@task = Task.create(params[:task])
+		@task.user_id = params[:user_id]
+		@task.save
+		flash[:notice] = " task created successfully"
+		redirect_to new_user_task_path
+	end
+
+	def show
+		#@task = @user.tasks.find(params[:id]) # with NESTING
+	end
+	
+	def edit
+	end
+
+	def update
+		@task.update_attributes(params[:task])
+		redirect_to user_path(@user)
+	end
+	
+	def destroy
+		@task.destroy
+		flash[:notice] = "task deleted successfully"
+		redirect_to user_path(@user)
+	end
+end
+
 =begin BEFORE NESTING TASKS with USERS
 	before_filter :get_task, :only => [:edit, :update, :show, :destroy]
 	before_filter :get_users, :only => [:edit, :new]
@@ -24,42 +62,3 @@ class TasksController < ApplicationController
 		@users = User.all # selecting user_id in forms
 	end
 =end
-	def index
-		@tasks = @user.tasks
-		#@tasks = Task.all BEFORE NESTING
-	end
-
-	def new
-		@task = Task.new
-		
-	end
-
-	def create
-		@task = Task.create(params[:task])
-		@task.user_id = params[:user_id]
-		@task.save
-		redirect_to user_path(@user)
-	end
-
-	def show
-		#@task = @user.tasks.find(params[:id]) # with NESTING
-		#@task = Task.find(params[:id])
-	end
-	
-	def edit
-		#@task = Task.find(params[:id])
-	end
-
-	def update
-		#@task = Task.find(params[:id])
-		@task.update_attributes(params[:task])
-		redirect_to user_path(@user)
-	end
-	
-	def destroy
-		#task = Task.find(params[:id])
-		@task.destroy
-		#Task.find(params[:id]).destroy
-		redirect_to user_path(@user)
-	end
-end
