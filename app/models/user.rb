756ad0ceed9c1621a1183265c55de2a860e5a2c3
brownmagik352 <asuman@Apurvs-MacBook-Pro.local>
2013-08-16 16:50:38 -0400

    add basic validations for user, minor view changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
   has_many :tasks
+
+  validates_presence_of :email, message: "no email"
+  validates_presence_of :name, message: "no name entered"
+  validates_length_of :name, in: 3..20, message: "name must be between 3 and 20 characters"
 end
